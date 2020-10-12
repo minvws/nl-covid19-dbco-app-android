e982e3762093e88ab58c74f887b87a9b64e7b4ca
insomnaicJustin <insomniac.robot@gmail.com>
2020-10-12 15:28:51 +0300

    DBCO-71: Updated onboarding flow again

--- app/src/main/java/nl/rijksoverheid/dbco/onboarding/FillCodeFragment.kt
+++ app/src/main/java/nl/rijksoverheid/dbco/onboarding/FillCodeFragment.kt
@@ -9,11 +9,14 @@
 package nl.rijksoverheid.dbco.onboarding
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import nl.rijksoverheid.dbco.BaseFragment
 import nl.rijksoverheid.dbco.R
 import nl.rijksoverheid.dbco.databinding.FragmentFillCodeBinding
+
 
 class FillCodeFragment : BaseFragment(R.layout.fragment_fill_code) {
 
@@ -24,5 +27,24 @@ class FillCodeFragment : BaseFragment(R.layout.fragment_fill_code) {
             findNavController().navigate(FillCodeFragmentDirections.toOnboardingAddDataFragment())
         }
 
+        binding.pinEntry1.setOnPinEnteredListener {
+            binding.pinEntry2.requestFocus()
+        }
+
+        binding.pinEntry2.setOnPinEnteredListener {
+            binding.pinEntry3.requestFocus()
+        }
+
+        binding.pinEntry3.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.btnNext.isEnabled = text?.length == 3
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
     }
 }
