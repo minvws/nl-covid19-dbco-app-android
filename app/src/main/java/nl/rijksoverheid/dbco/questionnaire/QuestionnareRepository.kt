f7f796b4d6f1507f63db7b9d96e7e2e8bf7cfd53
dima-ko <dima@egeniq.com>
2020-11-17 11:53:09 +0200

    Fixed questionnare storing

--- app/src/main/java/nl/rijksoverheid/dbco/questionnaire/QuestionnareRepository.kt
+++ app/src/main/java/nl/rijksoverheid/dbco/questionnaire/QuestionnareRepository.kt
@@ -9,6 +9,9 @@
 package nl.rijksoverheid.dbco.questionnaire
 
 import android.content.Context
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import nl.rijksoverheid.dbco.network.StubbedAPI
 import nl.rijksoverheid.dbco.questionnaire.data.entity.Questionnaire
 
@@ -16,11 +19,32 @@ class QuestionnareRepository(context: Context) : IQuestionnaireRepository {
 
     private val api = StubbedAPI.create(context)
     private var cachedQuestionnaire: Questionnaire? = null
+    private val sharedPrefs =
+        context.getSharedPreferences(PREFS_QUESTIONNAIRE, Context.MODE_PRIVATE)
 
     override suspend fun syncQuestionnaires() {
-        cachedQuestionnaire = api.getQuestionnaires().questionnaires?.firstOrNull() // TODO cache in shared prefs
+        if (cachedQuestionnaire != null) {
+            return
+        }
+        val storedQuestionnaire = sharedPrefs.getString(KEY_QUESTIONNAIRE, null)
+        if (storedQuestionnaire == null) {
+            cachedQuestionnaire = api.getQuestionnaires().questionnaires?.firstOrNull()
+            val questionnaireString = Json {
+                ignoreUnknownKeys = true
+            }.encodeToString(cachedQuestionnaire)
+            sharedPrefs.edit().putString(KEY_QUESTIONNAIRE, questionnaireString)
+                .apply()
+        } else {
+            cachedQuestionnaire = Json {
+                ignoreUnknownKeys = true
+            }.decodeFromString(storedQuestionnaire)
+        }
     }
 
     override fun getCachedQuestionnaire() = cachedQuestionnaire
 
+    companion object {
+        const val PREFS_QUESTIONNAIRE = "PREFS_QUESTIONNAIRE"
+        const val KEY_QUESTIONNAIRE = "KEY_QUESTIONNAIRE"
+    }
 }
