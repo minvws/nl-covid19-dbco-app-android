5e285b1ba4190b64ac2cff0cb18950079ae7c2f0
Jan Jaap de Groot <info@janjaap.com>
2021-01-21 10:49:19 +0100

    DBCO-638: Improve TalkBack & large font size interaction with code text fields

--- app/src/main/java/nl/rijksoverheid/dbco/util/Extensions.kt
+++ app/src/main/java/nl/rijksoverheid/dbco/util/Extensions.kt
@@ -12,8 +12,10 @@ import android.content.Context
 import android.content.res.Resources
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.xwray.groupie.ExpandableGroup
 import kotlinx.serialization.json.JsonPrimitive
+import nl.rijksoverheid.dbco.onboarding.FillCodeField
 
 fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
 fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
@@ -56,3 +58,16 @@ fun ExpandableGroup.removeAllChildren() {
 
 @ExperimentalUnsignedTypes
 fun ByteArray.toHexString() = asUByteArray().joinToString("") { it.toString(16).padStart(2, '0') }
+
+fun EditText.updateText(text: CharSequence) {
+    setText(text)
+    setSelection(text.length)
+}
+
+fun CharSequence.numbers(limit: Int? = null): String {
+    val numbers = replace("[^0-9]".toRegex(), "")
+    return when (limit != null) {
+        true -> numbers.take(limit)
+        false -> numbers
+    }
+}
