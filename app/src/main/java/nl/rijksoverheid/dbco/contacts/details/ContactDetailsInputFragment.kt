a0539dbafc07030bb67c25e3097ae710ea119328
NielsMasdorp <nielsmasdorp@gmail.com>
2021-03-02 09:17:59 +0100

    Fix bug where it was not possible to add more then one contact in the app during the regular flow

--- app/src/main/java/nl/rijksoverheid/dbco/contacts/details/ContactDetailsInputFragment.kt
+++ app/src/main/java/nl/rijksoverheid/dbco/contacts/details/ContactDetailsInputFragment.kt
@@ -77,10 +77,10 @@ class ContactDetailsInputFragment : BaseFragment(R.layout.fragment_contact_input
             viewLifecycleOwner,
             appLifecycleViewModel.getFeatureFlags()
         ).apply {
-            if(task.source != Source.Portal) {
+            if (task.source != Source.Portal) {
                 // If the task is coming from the portal remove the classification section and change the other section's numbering accordingly
                 adapter.add(classificationSection)
-            }else{
+            } else {
                 contactDetailsSection.setSectionNumber(1)
                 informSection.setSectionNumber(2)
             }
@@ -125,14 +125,14 @@ class ContactDetailsInputFragment : BaseFragment(R.layout.fragment_contact_input
             itemsStorage?.refreshInformSection()
 
             binding.saveButton.text =
-            if (it == Category.NO_RISK && task.source == Source.App && task.uuid != null) {
-                getString(R.string.delete)
-            } else  if (it == Category.NO_RISK && task.source == Source.App && task.uuid == null || it == null) {
-                // Show cancel if contact is not at risk and not saved yet, or if no category is set yet
-                getString(R.string.cancel)
-            } else {
-                getString(R.string.save)
-            }
+                if (it == Category.NO_RISK && task.source == Source.App && task.uuid != null) {
+                    getString(R.string.delete)
+                } else if (it == Category.NO_RISK && task.source == Source.App && task.uuid == null || it == null) {
+                    // Show cancel if contact is not at risk and not saved yet, or if no category is set yet
+                    getString(R.string.cancel)
+                } else {
+                    getString(R.string.save)
+                }
         })
 
         viewModel.communicationType.observe(viewLifecycleOwner, {
@@ -160,7 +160,7 @@ class ContactDetailsInputFragment : BaseFragment(R.layout.fragment_contact_input
             // If this happens, the app can crash due to the RV not accepting changes during layout computes
             // Adding this check makes sure the section is only refreshed when the user is actively filling
             // in their data rather than during a scroll.
-            if(!binding.content.isComputingLayout) {
+            if (!binding.content.isComputingLayout) {
                 itemsStorage?.refreshInformSection()
             }
         })
@@ -172,7 +172,7 @@ class ContactDetailsInputFragment : BaseFragment(R.layout.fragment_contact_input
                     // Or, if it hasn't been saved yet, cancel. If the task is valid, save instead
                 if (it == ANSWER_EARLIER && task.source == Source.App && task.uuid != null) {
                     getString(R.string.delete)
-                } else if (it == ANSWER_EARLIER && task.source == Source.App && task.uuid == null || viewModel.category.value == null ) {
+                } else if (it == ANSWER_EARLIER && task.source == Source.App && task.uuid == null || viewModel.category.value == null) {
                     // If the date is before the infection period OR the category isn't set yet, show cancel. Required to stop overriding by observers on category and date
                     getString(R.string.cancel)
                 } else {
@@ -227,13 +227,13 @@ class ContactDetailsInputFragment : BaseFragment(R.layout.fragment_contact_input
         builder.create().show()
     }
 
-    private fun showUnsavedChangesDialog(view: View){
+    private fun showUnsavedChangesDialog(view: View) {
         val builder = AlertDialog.Builder(view.context)
         builder.setMessage(getString(R.string.unsaved_changes_message))
         builder.setPositiveButton(R.string.save) { dialog, _ ->
             if (viewModel.communicationType.value == CommunicationType.Index && viewModel.task.value?.didInform == false) {
                 showDidYouInformDialog(view)
-            }else{
+            } else {
                 collectAnswers()
             }
             dialog.dismiss()
@@ -325,7 +325,7 @@ class ContactDetailsInputFragment : BaseFragment(R.layout.fragment_contact_input
                             viewModel.category.value?.let { category ->
                                 map["value"] = JsonPrimitive(category.label)
                             }
-                            value =  JsonObject(map)
+                            value = JsonObject(map)
                         }
                         val answer = Answer(
                             UUID.randomUUID().toString(),
@@ -355,8 +355,12 @@ class ContactDetailsInputFragment : BaseFragment(R.layout.fragment_contact_input
             if (task.uuid.isNullOrEmpty()) {
                 task.uuid = UUID.randomUUID().toString()
             }
-            if(task.label.isNullOrEmpty()){
-                task.label = getString(R.string.mycontacts_name_unknown)
+            if (task.label.isNullOrEmpty()) {
+                if (!task.linkedContact?.getDisplayName().isNullOrEmpty()) {
+                    task.label = task.linkedContact?.getDisplayName()
+                } else {
+                    task.label = getString(R.string.mycontacts_name_unknown)
+                }
             }
             answers.firstOrNull { it.questionUuid == CONTACT_TYPE_UUID }?.value?.get("value")?.jsonPrimitive?.content.let {
                 task.taskContext = it
